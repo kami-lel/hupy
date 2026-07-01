4e80f77616ae90df1944b2a8b9df1db820302d5a
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-01 13:07:14 +0800

    add commit_type module classify in-progress git commit states
    ^`commit_type.py`

---
+++ hupy/commit_type.py
@@ -0,0 +1,79 @@
+"""
+commit_type.py
+
+identify the type of an in-progress git commit
+"""
+
+import os
+from enum import Flag, auto
+
+import git
+
+# helpers  #####################################################################
+
+
+class CommitType(Flag):  # =====================================================
+    """
+    represent the type of an in-progress git commit with two-level
+    hierarchy: level 1 distinguishes merges from other commits;
+    level 2 further categorizes merges by strategy
+    """
+
+    _MERGE = auto()
+    _OTHER_COMMIT = auto()
+    _FEATURE_FINISH = auto()
+    _VERSION_RELEASE = auto()
+    _OTHER_MERGE = auto()
+    _CHERRY_PICK = auto()
+    _REVERT = auto()
+    _INITIAL = auto()
+    _REGULAR = auto()
+
+    # Public Instances  --------------------------------------------------------
+
+    MERGE = _MERGE
+    OTHER_COMMIT = _OTHER_COMMIT
+
+    FEATURE_FINISH = _MERGE | _FEATURE_FINISH
+    VERSION_RELEASE = _MERGE | _VERSION_RELEASE
+    OTHER_MERGE = _MERGE | _OTHER_MERGE
+
+
+def _has_state(git_dir, name):
+    return os.path.isfile(os.path.join(git_dir, name))
+
+
+# Public API ###################################################################
+def get_current_commit_type(repo_path="."):
+    """
+    return the type of the current in-progress commit
+
+
+    :param repo_path: path to the git repository or any of its
+            subdirectories; defaults to the current directory
+    :type repo_path: str
+    :raises git.InvalidGitRepositoryError: if no git repository is
+            found at or above ``repo_path``
+    :raises git.NoSuchPathError: if ``repo_path`` does not exist
+    :return: a ``CommitType`` member identifying the in-progress
+            commit; one of ``FEATURE_FINISH``, ``VERSION_RELEASE``,
+            ``OTHER_MERGE``, ``CHERRY_PICK``, ``REVERT``, ``INITIAL``,
+            or ``REGULAR``
+    :rtype: CommitType
+    :example:
+    >>> get_current_commit_type()
+    <CommitType.REGULAR: ...>
+    """
+    repo = git.Repo(repo_path, search_parent_directories=True)
+    gd = repo.git_dir
+    if _has_state(gd, "MERGE_HEAD"):
+        return CommitType.OTHER_MERGE
+    if _has_state(gd, "CHERRY_PICK_HEAD"):
+        return CommitType.CHERRY_PICK
+    if _has_state(gd, "REVERT_HEAD"):
+        return CommitType.REVERT
+    try:
+        repo.head.commit  # raises ValueError on empty repo
+    except ValueError:
+        return CommitType.INITIAL
+    return CommitType.REGULAR
