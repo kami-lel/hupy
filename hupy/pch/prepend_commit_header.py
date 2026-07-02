94c17d927dedd149a20e5ddf08e0b84bd6846bcf
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-02 18:44:07 +0800

    implement commit header prepend via repo COMMIT_EDITMSG rewrite
    +`prepend_commit_header.py`

--- hupy/pch/prepend_commit_header.py
+++ hupy/pch/prepend_commit_header.py
@@ -4,6 +4,11 @@ prepend_commit_header.py
 prepend commit type header to commit message
 """
 
+import os
+import tempfile
+
+import git
+
 from hupy.kamilog import getLogger
 from . import PCH_LOGGER_NAME
 from ..commit_type import CommitType, get_current_commit_type
@@ -15,9 +20,52 @@ logger = getLogger(PCH_LOGGER_NAME)
 # helpers  #####################################################################
 
 
-def _prepend_commit_header_by_type(is_feature_finish):
-    # TODO implement commit header prepending
-    pass
+# BUG probably wrong
+def _prepend_commit_header_by_type(is_feature_finish, repo_root):
+    """
+    prepend a header line and a blank line to the commit message file.
+
+
+    :param is_feature_finish: ``True`` for a Feature Finish merge,
+            ``False`` for a Version Release merge
+    :type is_feature_finish: bool
+    :param repo_root: path to the git repository or any of its
+            subdirectories
+    :type repo_root: str
+    """
+    repo = git.Repo(repo_root, search_parent_directories=True)
+    commit_editmsg_path = os.path.join(repo.git_dir, "COMMIT_EDITMSG")
+
+    content_lines = []
+    comment_lines = []
+
+    with open(commit_editmsg_path, encoding="utf-8") as f:
+        for line in f:
+            line = line.rstrip("\n")
+            target = (
+                comment_lines
+                if line.lstrip().startswith("#")
+                else content_lines
+            )
+            target.append(line)
+    logger.debug("content_lines=%r", content_lines)
+
+    header = ""
+    content_lines = [header, ""] + content_lines
+    logger.debug("content_lines=%r", content_lines)
+
+    directory = os.path.dirname(commit_editmsg_path) or "."
+    fd, tmp_path = tempfile.mkstemp(dir=directory, prefix="commit-msg.")
+
+    try:
+        with os.fdopen(fd, "w", encoding="utf-8") as f:
+            f.write("\n".join(content_lines))
+            f.write("\n")
+            f.write("\n".join(comment_lines))
+        os.replace(tmp_path, commit_editmsg_path)
+    except BaseException:
+        os.unlink(tmp_path)
+        raise
 
 
 # Public API  ##################################################################
@@ -28,7 +76,8 @@ def prepend_commit_header(repo_root):
     prepend commit type header to the current commit message.
 
 
-    :param repo_root: path to the git repository or any of its subdirectories
+    :param repo_root: path to the git repository or any of its
+            subdirectories
     :type repo_root: str
     """
     logger.enter("prepending commit header")
@@ -37,11 +86,11 @@ def prepend_commit_header(repo_root):
 
     if CommitType.FEATURE_FINISH in commit_type:
         logger.info("prepending header on Feature Finish merge")
-        _prepend_commit_header_by_type(is_feature_finish=True)
+        _prepend_commit_header_by_type(True, repo_root)
 
     elif CommitType.VERSION_RELEASE in commit_type:
         logger.info("prepending header on Version Release merge")
-        _prepend_commit_header_by_type(is_feature_finish=False)
+        _prepend_commit_header_by_type(False, repo_root)
 
     else:
         logger.skip("regular commit/merge")
