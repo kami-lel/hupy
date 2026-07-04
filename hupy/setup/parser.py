66e1b48bba7926da75eb3e27aaeba421c5328f52
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-05 01:40:24 +0800

    add CLI args repo_root/--hooks-dir to HUPy init parser
    /`parser.py`

--- hupy/setup/parser.py
+++ hupy/setup/parser.py
@@ -1,5 +1,9 @@
 """initialize HUPy in the current repository"""
 
+import argparse
+import os
+import pathlib
+
 from hupy.setup import SETUP_LOGGER_NAME
 
 
@@ -12,6 +16,20 @@ from hupy.kamilog import (
 # logger  ######################################################################
 
 logger = getLogger(SETUP_LOGGER_NAME)
+
+
+# constants  ###################################################################
+
+
+_DESCRIPTION = __doc__ + """
+
+performs:
+
+- copies the default HUPy hooks scripts into REPO_ROOT/scripts/hupy-hooks/,
+  (or into --hooks-dir)
+- configures git to run hooks from above path,
+  instead of default untracked .git/hooks/
+"""
 
 # helpers  #####################################################################
 
@@ -27,9 +45,12 @@ def _init_main(args):
     set_logging_level_by_verbosity(args, logger=logger)
 
     logger.enter("HUPy Initialization")
-    root_path = "abc"
+    root_path = args.repo_root
+    hooks_dir = args.hooks_dir or root_path / "scripts" / "hupy-hooks"
     # TODO TODO mpl setups
-    logger.done("HUPy Initialized for: {}".format(root_path))
+    logger.done(
+        "HUPy Initialized for: {} (hooks dir: {})".format(root_path, hooks_dir)
+    )
 
 
 # Public API  ##################################################################
@@ -40,7 +61,29 @@ def register_cli_init_parser(cli_subparser):
     init_parser = cli_subparser.add_parser(
         "init",
         help=__doc__,
-        description=__doc__,
+        description=_DESCRIPTION,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    init_parser.add_argument(
+        "repo_root",
+        metavar="REPO_ROOT",
+        nargs="?",
+        type=pathlib.Path,
+        default=pathlib.Path(os.getcwd()),
+        help=(
+            "path to the git repository to initialize; "
+            "default=current working directory"
+        ),
+    )
+
+    init_parser.add_argument(
+        "--hooks-dir",
+        dest="hooks_dir",
+        metavar="HOOKS_DIR",
+        type=pathlib.Path,
+        default=None,
+        help="specify alternative folder to place all HUPy hooks scripts",
     )
 
     add_verbose_arguments(init_parser)
