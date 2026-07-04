b8e94dd2641ae7eecc3a6203c2f760edf9b83882
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-05 01:22:00 +0800

    wire init subcommand to Verbosity logging, set help from __doc__
    /`parser.py`

--- hupy/setup/parser.py
+++ hupy/setup/parser.py
@@ -1,8 +1,19 @@
-"""
-parser.py
+"""initialize HUPy in the current repository"""
 
-init CLI parser and subcommand registration
-"""
+from hupy.setup import SETUP_LOGGER_NAME
+
+
+from hupy.kamilog import (
+    add_verbose_arguments,
+    set_logging_level_by_verbosity,
+    getLogger,
+)
+
+# logger  ######################################################################
+
+logger = getLogger(SETUP_LOGGER_NAME)
+
+# helpers  #####################################################################
 
 
 def _init_main(args):
@@ -13,15 +24,21 @@ def _init_main(args):
     :param args: parsed arguments from argparse
     :type args: argparse.Namespace
     """
-    pass  # TODO TODO mpl setups
+    set_logging_level_by_verbosity(SETUP_LOGGER_NAME)
+    # TODO TODO mpl setups
 
 
+# Public API  ##################################################################
 def register_cli_init_parser(cli_subparser):
     """
     register the ``init`` subcommand parser.
     """
     init_parser = cli_subparser.add_parser(
         "init",
-        help="initialize HUPy in the current repository",
+        help=__doc__,
+        description=__doc__,
     )
+
+    add_verbose_arguments(init_parser)
+
     init_parser.set_defaults(func=_init_main)
