d46d0d168cc239cddd25075cb557d87973e1bedd
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-05 22:45:47 +0800

    refactor CLI parser registration; add stage subparser helpers
    +`cli.py` refactor CLI parser wiring: new per-stage parser functions
    /`pyproject.toml` update project description punctuation for clearer Hooks Utility Python

--- hupy/cli.py
+++ hupy/cli.py
@@ -1,8 +1,4 @@
-"""
-cli.py
-
-main argument parser for the hupy command-line interface
-"""
+"""CLI of HUPy (Hooks Utility Python): git hook utilities for commit quality enforcement"""
 
 from argparse import ArgumentParser
 
@@ -12,105 +8,128 @@ from hupy.ttg.parser import register_cli_ttg_parser
 
 __all__ = ("cli_parser", "cli_subparser")
 
-# main parse  ##################################################################
+
+# main parser  #################################################################
 
 
-def _cli_main(_):
-    """
-    dispatch when calling ``python -m hupy`` without subcommand.
+cli_parser = ArgumentParser(
+    prog=__package__,
+    description=__doc__,
+)
+cli_parser.set_defaults(func=lambda _: cli_parser.print_help())
+cli_subparser = cli_parser.add_subparsers(title="subcommands")
 
 
-    :param _: parsed arguments (unused)
-    :type _: argparse.Namespace
-    """
-    cli_parser.print_help()
+# pre-commit subparser  ########################################################
 
 
-# pre-commit stage  ############################################################
+_PRE_COMMIT_DOC = "utilities for pre-commit git hook stage"
 
 
-def _pre_commit_main(args):
-    """
-    dispatch when calling ``pre-commit`` without a subcommand.
+def _register_pre_commit_parser(subparser):
+    pre_commit_parser = subparser.add_parser(
+        "pre-commit",
+        help=_PRE_COMMIT_DOC,
+        description=_PRE_COMMIT_DOC,
+    )
+    pre_commit_parser.set_defaults(
+        func=lambda _: pre_commit_parser.print_help(), parser=pre_commit_parser
+    )
+
+    pre_commit_subparser = pre_commit_parser.add_subparsers(title="subcommands")
+
+    _register_pre_commit_start_parser(pre_commit_subparser)
+    register_cli_ttg_parser(pre_commit_subparser)
+    _register_pre_commit_end_parser(pre_commit_subparser)
 
 
-    :param args: parsed arguments from argparse
-    :type args: argparse.Namespace
-    """
-    args.parser.print_help()
+# pre-commit start  ============================================================
+
+_PRE_COMMIT_START_DOC = "mark start of pre-commit stage"
 
 
 def _pre_commit_start_main(_):
     """
     dispatch for the ``pre-commit start`` subcommand.
-
-
-    :param _: parsed arguments (unused)
-    :type _: argparse.Namespace
     """
     pass  # Todo mpl pre-commit start
+
+
+def _register_pre_commit_start_parser(subparser):
+    start_parser = subparser.add_parser(
+        "start", help=_PRE_COMMIT_START_DOC, description=_PRE_COMMIT_START_DOC
+    )
+    start_parser.set_defaults(func=_pre_commit_start_main)
+
+
+# pre-commit end  ==============================================================
+
+_PRE_COMMIT_END_DOC = "mark end of pre-commit stage"
 
 
 def _pre_commit_end_main(_):
     """
     dispatch for the ``pre-commit end`` subcommand.
-
-
-    :param _: parsed arguments (unused)
-    :type _: argparse.Namespace
     """
     pass  # Todo mpl pre-commit end
 
 
-def _register_cli_pre_commit_parser(cli_subparser):
-    """
-    register the ``pre-commit`` subcommand group and its subcommands.
-    """
-    pre_commit_parser = cli_subparser.add_parser(
-        "pre-commit",
-        help="run utilities scoped to the pre-commit git hook stage",
-    )
-    pre_commit_parser.set_defaults(
-        func=_pre_commit_main, parser=pre_commit_parser
-    )
-    pre_commit_subparser = pre_commit_parser.add_subparsers(title="subcommands")
-
-    start_parser = pre_commit_subparser.add_parser(
-        "start", help="mark the start of the pre-commit stage"
-    )
-    start_parser.set_defaults(func=_pre_commit_start_main)
-
-    register_cli_ttg_parser(pre_commit_subparser)
-
-    end_parser = pre_commit_subparser.add_parser(
-        "end", help="mark the end of the pre-commit stage"
+def _register_pre_commit_end_parser(subparser):
+    end_parser = subparser.add_parser(
+        "end", help=_PRE_COMMIT_END_DOC, description=_PRE_COMMIT_END_DOC
     )
     end_parser.set_defaults(func=_pre_commit_end_main)
 
 
-# prepare-commit-msg stage  ####################################################
+# prepare-commit-msg subparser  ################################################
+
+_PREPARE_COMMIT_MSG_DOC = "utilities for prepare-commit-msg git hook stage"
 
 
-def _prepare_commit_msg_main(args):
-    """
-    dispatch when calling ``prepare-commit-msg`` without a subcommand.
+def _register_prepare_commit_msg_parser(subparser):
+    prepare_commit_msg_parser = subparser.add_parser(
+        "prepare-commit-msg",
+        help=_PREPARE_COMMIT_MSG_DOC,
+        description=_PREPARE_COMMIT_MSG_DOC,
+    )
+    prepare_commit_msg_parser.set_defaults(
+        func=lambda _: prepare_commit_msg_parser.print_help(),
+        parser=prepare_commit_msg_parser,
+    )
+
+    prepare_commit_msg_subparser = prepare_commit_msg_parser.add_subparsers(
+        title="subcommands"
+    )
+
+    _register_prepare_commit_msg_start_parser(prepare_commit_msg_subparser)
+    register_cli_pch_parser(prepare_commit_msg_subparser)
+    _register_prepare_commit_msg_end_parser(prepare_commit_msg_subparser)
 
 
-    :param args: parsed arguments from argparse
-    :type args: argparse.Namespace
-    """
-    args.parser.print_help()
+# prepare-commit-msg start  =====================================================
+
+_PREPARE_COMMIT_MSG_START_DOC = "mark start of prepare-commit-msg stage"
 
 
 def _prepare_commit_msg_start_main(_):
     """
     dispatch for the ``prepare-commit-msg start`` subcommand.
-
-
-    :param _: parsed arguments (unused)
-    :type _: argparse.Namespace
     """
     pass  # Todo mpl prepare-commit-msg start
+
+
+def _register_prepare_commit_msg_start_parser(subparser):
+    start_parser = subparser.add_parser(
+        "start",
+        help=_PREPARE_COMMIT_MSG_START_DOC,
+        description=_PREPARE_COMMIT_MSG_START_DOC,
+    )
+    start_parser.set_defaults(func=_prepare_commit_msg_start_main)
+
+
+# prepare-commit-msg end  =======================================================
+
+_PREPARE_COMMIT_MSG_END_DOC = "mark end of prepare-commit-msg stage"
 
 
 def _prepare_commit_msg_end_main(_):
@@ -124,46 +143,19 @@ def _prepare_commit_msg_end_main(_):
     pass  # Todo mpl prepare-commit-msg end
 
 
-def _register_cli_prepare_commit_msg_parser(cli_subparser):
-    """
-    register the ``prepare-commit-msg`` subcommand group and its
-    subcommands.
-    """
-    prepare_commit_msg_parser = cli_subparser.add_parser(
-        "prepare-commit-msg",
-        help="run utilities scoped to the prepare-commit-msg git hook stage",
-    )
-    prepare_commit_msg_parser.set_defaults(
-        func=_prepare_commit_msg_main, parser=prepare_commit_msg_parser
-    )
-    prepare_commit_msg_subparser = prepare_commit_msg_parser.add_subparsers(
-        title="subcommands"
-    )
-
-    start_parser = prepare_commit_msg_subparser.add_parser(
-        "start", help="mark the start of the prepare-commit-msg stage"
-    )
-    start_parser.set_defaults(func=_prepare_commit_msg_start_main)
-
-    register_cli_pch_parser(prepare_commit_msg_subparser)
-
-    end_parser = prepare_commit_msg_subparser.add_parser(
-        "end", help="mark the end of the prepare-commit-msg stage"
+def _register_prepare_commit_msg_end_parser(subparser):
+    end_parser = subparser.add_parser(
+        "end",
+        help=_PREPARE_COMMIT_MSG_END_DOC,
+        description=_PREPARE_COMMIT_MSG_END_DOC,
     )
     end_parser.set_defaults(func=_prepare_commit_msg_end_main)
 
 
-cli_parser = ArgumentParser(
-    prog=__package__,
-    description=__doc__,
-)
-cli_parser.set_defaults(func=_cli_main)
-cli_subparser = cli_parser.add_subparsers(title="subcommands")
-
-# register subcommands
+# register subparsers  #########################################################
 register_cli_init_parser(cli_subparser)
-_register_cli_pre_commit_parser(cli_subparser)
-_register_cli_prepare_commit_msg_parser(cli_subparser)
+_register_pre_commit_parser(cli_subparser)
+_register_prepare_commit_msg_parser(cli_subparser)
 
 
 # Todo find version feature
