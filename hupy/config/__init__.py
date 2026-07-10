5b6a8f6a8ba4dd34f7d95dea9c06ac0de5575001
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-10 17:05:45 +0800

    add TODO for JsonC config update and versioned script
    #`__init__.py`

--- hupy/config/__init__.py
+++ hupy/config/__init__.py
@@ -9,4 +9,5 @@ from hupy import PROJ_LOGGER_NAME
 CONFIG_FILENAME = ".hupy.config.json"
 CONFIG_LOGGER_NAME = PROJ_LOGGER_NAME + ".config-file"
 
+# todo consider using jsonc
 # todo script to upd config file per version
