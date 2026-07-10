c2ed02fdebb34abe5aaed90032946a6be260ebcd
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-11 01:34:18 +0800

    add state-file path resolver; drop cli TODO
    #`cli_main.py` remove stale TODO comment about state file
    ^`open_state.py` add open_state.py stub; TODO Context Manager Open State file
    ^`state_file_path.py` add state_file_path helper for hupy-state.json in repo .git

--- hupy/cli/cli_main.py
+++ hupy/cli/cli_main.py
@@ -36,7 +36,6 @@ register_cli_prepare_commit_msg_parser(cli_subparser)
 register_cli_verify_parser(cli_subparser)
 
 
-# TODO add state file
 # TODO allow tmp skip some feature for next round
 
 # todo dry run feature for hooks CLI
---
+++ hupy/state/open_state.py
@@ -0,0 +1,1 @@
+# TODO ctxt manager open state file
