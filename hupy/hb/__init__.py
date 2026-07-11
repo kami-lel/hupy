5e03b50d70b12253b43137d9fe41eaba81867a67
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-11 13:53:39 +0800

    add Hb Constants section and keep HB_LOGGER_NAME export
    #`__init__.py`

--- hupy/hb/__init__.py
+++ hupy/hb/__init__.py
@@ -7,4 +7,7 @@ from hupy import PROJ_LOGGER_NAME
 __all__ = ("HB_LOGGER_NAME",)
 
 
+# constants  ###################################################################
+
+
 HB_LOGGER_NAME = PROJ_LOGGER_NAME + ".HB"
