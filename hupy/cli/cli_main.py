03299de5df85fcfa15ecc6d05c46d27410651b3c
kamiLeL <77594253+kami-lel@users.noreply.github.com>
2026-07-27 01:42:39 +0800

    remove orphaned TODO comment about dry run feature
    !`cli_main.py`

--- hupy/cli/cli_main.py
+++ hupy/cli/cli_main.py
@@ -40,6 +40,3 @@ register_cli_uninstall_parser(cli_subparser)
 register_cli_verify_parser(cli_subparser)
 register_cli_accessors_parser(cli_subparser)
 register_cli_hook_parser(cli_subparser)
-
-
-# Todo dry run feature in CLI
